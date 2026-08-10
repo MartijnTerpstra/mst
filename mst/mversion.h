9c548c0e9c39de64abd4c7d8a7632a16e0bbe51b
Martijn Terpstra <m-terpstra@outlook.com>
2026-08-10 02:19:47 +0200

    Move to V4.4.00

--- mst/mversion.h
+++ mst/mversion.h
@@ -26,10 +26,10 @@
 #pragma once
 
 /* the current version of the MST */
-#define MST_VER 4301
+#define MST_VER 4400
 
 /* the current version of the MST in string format */
-#define MST_VER_STR "4.3.01"
+#define MST_VER_STR "4.4.00"
 
 /**/
 /* when this version is changed, functionality is changed or deprecated */
